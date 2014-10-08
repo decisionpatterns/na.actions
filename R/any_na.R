569c84c5774126c0abe57a1282eeb68fadfdcbc2
Christopher Brown <cbrown@decisionpatterns.com>
2014-10-08 00:13:58 -0700

    Add R/any_na.R

---
+++ R/any_na.R
@@ -0,0 +1,32 @@
+#' Determine if values are mission
+#' 
+#' Determines if any of the values in a data structure are missing  
+#'
+#' @name any_na
+#' @param x any object
+#' 
+#' @examples
+#'   any_na( 1:10 )           # FALSE
+#'   any_na( c( 1, NA, 3 ) )  # TRUE
+#'   
+#' @export
+
+any_na <- function(x) UseMethod("any_na")
+
+#' @return \code{logical}
+#' @rdname any_na
+#' @method any_na default
+#' @aliases any_na.default
+#' @export
+
+any_na.default <- function(x)
+   any( is.na( x ) )
+
+
+#' @return \code{logical}
+#' @rdname any_na
+#' @method any_na data.frame
+#' @aliases any_na.data.frame
+#' @export
+any_na.data.frame <- function(x)
+  any( sapply( x, any_na ) )
