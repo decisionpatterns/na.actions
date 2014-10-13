fd9d0413ea5dc706032541de2fd858c9c1130bbc
Christopher Brown <cbrown@decisionpatterns.com>
2014-10-13 11:31:37 -0700

    Add na.roughfix.POSIXct from `dp.misc` package

---
+++ R/na.roughfix.POSIXct.R
@@ -0,0 +1,20 @@
+#' na.roughfix.POXIXct - replace a date/time field 
+#' 
+#' @param object to replace \code{NA}s with
+#' 
+#' @export
+
+na.roughfix.POSIXct <- function(object) { 
+
+  dts <- classes( object ) == 'POSIXct'
+  wh <- dts[dts]
+  for( w in names(wh) ) {
+    x <- object[[w]]
+    if( any_na(x) ) 
+      object[[w]] <- na_replace( x, median(x, na.rm=TRUE ))
+    
+  }
+
+  return(object)
+  
+}
