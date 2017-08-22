2bca9d168965e70b0965a47dca61a9024a83a85a
Christopher Brown <cbrown@decisionpatterns.com>
2017-08-22 13:55:20 -0700

    v0.7.1: Fix na_all to data.frames

--- R/na_all.R
+++ R/na_all.R
@@ -25,6 +25,12 @@
 #'   na_all( c( NA, NA, 1 ) ) # FALSE
 #'   na_all( c( NA, NA, NA ) )   # TRUE
 #'   
+#'   df <- data.frame( char = rep(NA_character_, 3), nums=1:3)
+#'   na_all(df)  # FALSE
+#'   
+#'   df <- data.frame( char = rep(NA_character_, 3), nums=rep(NA_real_,3))
+#'   na_all(df)  # TRUE
+#'   
 #' @export
 
 na_all <- function(x) UseMethod("na_all")
@@ -44,8 +50,9 @@ na_all.default <- function(x)
 #' @method na_all data.frame
 #' @aliases na_all.data.frame
 #' @export
+
 na_all.data.frame <- function(x)
-  any( sapply( x, na_all ) )
+  all( sapply( x, na_all ) )
 
 
 #' @rdname na_all 
